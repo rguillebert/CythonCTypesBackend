60430b00256c3831f3d7ba9005cbb7af76e23fdb
Romain Guillebert <romain.py@gmail.com>
2011-08-13 04:48:54 +0200

    New cdefed variable arithmetic

--- Cython/CTypesBackend/CDefVarManipulationTransform.py
+++ Cython/CTypesBackend/CDefVarManipulationTransform.py
@@ -1,6 +1,6 @@
 from Cython.Compiler.Visitor import VisitorTransform
 from Cython.Compiler.Nodes import SingleAssignmentNode, CascadedAssignmentNode, InPlaceAssignmentNode, ParallelAssignmentNode
-from Cython.Compiler.ExprNodes import AttributeNode
+from Cython.Compiler.ExprNodes import AttributeNode, NameNode
 from Cython.Compiler.PyrexTypes import CStructOrUnionType
 from Cython.Compiler.TreeFragment import TreeFragment
 
@@ -29,43 +29,35 @@ class CDefVarManipulationTransform(VisitorTransform):
     def is_basic_C_type(self, node):
         return node.type != None and (node.type.is_numeric or node.type.is_string)
 
-    def wrap(self, node):
-        """ Wraps the rhs into the ctypes type of the lhs """
-        if hasattr(node.lhs.type, 'sign_and_name') and node.lhs.type.sign_and_name() in self.c_type_to_ctypes_type_dict.keys():
-            tf = TreeFragment(u'ctypes.c_%s(DUMMY)' % (self.c_type_to_ctypes_type_dict[node.lhs.type.sign_and_name()])).root.stats[0].expr
-            tf.args = [node.rhs]
-            node.rhs = tf
-        else:
-            tf = TreeFragment(u'%s()' % unicode(node.lhs.type.name)).root.stats[0].expr
-            node.rhs = tf
-        return node
+#    def visit_AssignmentNode(self, node):
+#        if isinstance(node, SingleAssignmentNode) or isinstance(node, InPlaceAssignmentNode):
+#            if not (self.is_structure_field(node.lhs) or self.is_arg(node.lhs)) and self.is_basic_C_type(node.lhs):
+#                node.lhs = AttributeNode(0, obj=node.lhs, attribute=u"value")
+#        elif isinstance(node, CascadedAssignmentNode):
+#            new_lhs_list = []
+#            for lhs in node.lhs_list:
+#                if not (self.is_structure(lhs) or self.is_arg(lhs)) and self.is_basic_C_type(lhs):
+#                    new_lhs_list.append(AttributeNode(0, obj=lhs, attribute=u"value"))
+#                else:
+#                    new_lhs_list.append(lhs)
+#            node.lhs_list = new_lhs_list
+#        elif isinstance(node, ParallelAssignmentNode):
+#            new_stats = []
+#            for assnode in node.stats:
+#                new_stats.append(self.visit_AssignmentNode(assnode))
+#            node.stats = new_stats
+#        self.visitchildren(node)
+#        return node
 
-    def visit_AssignmentNode(self, node):
-        if isinstance(node, SingleAssignmentNode) or isinstance(node, InPlaceAssignmentNode):
-            if not (self.is_structure_field(node.lhs) or self.is_arg(node.lhs)) and self.is_basic_C_type(node.lhs):
-                node.lhs = AttributeNode(0, obj=node.lhs, attribute=u"value")
-            elif self.is_arg(node.lhs):
-                node = self.wrap(node)
-        elif isinstance(node, CascadedAssignmentNode):
-            new_lhs_list = []
-            for lhs in node.lhs_list:
-                if not (self.is_structure(lhs) or self.is_arg(lhs)) and self.is_basic_C_type(lhs):
-                    new_lhs_list.append(AttributeNode(0, obj=lhs, attribute=u"value"))
-                else:
-                    new_lhs_list.append(lhs)
-            node.lhs_list = new_lhs_list
-        elif isinstance(node, ParallelAssignmentNode):
-            new_stats = []
-            for assnode in node.stats:
-                new_stats.append(self.visit_AssignmentNode(assnode))
-            node.stats = new_stats
-        self.visitchildren(node)
+    def visit_NameNode(self, node):
+        if self.is_basic_C_type(node):
+            return AttributeNode(0, obj=node, attribute=u"value")
         return node
 
     def visit_CoerceToPyTypeNode(self, node):
-        self.visitchildren(node.arg)
+        self.visitchildren(node)
         return node.arg
 
     def visit_CoerceFromPyTypeNode(self, node):
-        self.visitchildren(node.arg)
+        self.visitchildren(node)
         return node.arg
